df0f206e73e69198077a21463aff1b24394e45d7
wonae789 <wonae789@naver.com>
2019-08-17 15:48:42 +0900

    연결리스트 테스트

---
+++ ALGORITHM_STUDY/ALGORITHM_STUDY/test.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+//int add(int x, int y)
+//{
+//	return x + y;
+//}
+
+typedef struct abc
+{
+	int a[22];
+	
+	int b[33];
+}abc;
+
+void print(abc point)
+{
+
+}
+
+int main(void)
+{
+	abc ddd;
+	print(ddd);
+
+
+
+	return 0;
+}
