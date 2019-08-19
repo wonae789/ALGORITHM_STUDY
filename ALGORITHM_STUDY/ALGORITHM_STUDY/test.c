693befc1baf1d43ff18b8c7f28820523a3beed69
wonae789 <wonae789@naver.com>
2019-08-19 11:04:37 +0900

    테스트

--- ALGORITHM_STUDY/ALGORITHM_STUDY/test.c
+++ ALGORITHM_STUDY/ALGORITHM_STUDY/test.c
@@ -1,28 +1,39 @@
 #include<stdio.h>
 
-//int add(int x, int y)
-//{
-//	return x + y;
-//}
-
-typedef struct abc
-{
-	int a[22];
-	
-	int b[33];
-}abc;
-
-void print(abc point)
-{
-
-}
-
 int main(void)
 {
-	abc ddd;
-	print(ddd);
 
-
-
-	return 0;
+	int *ptr;
+	*ptr = 17;
 }
+
+//struct point
+//{
+//	int xpos;
+//	int ypos;
+//};
+//
+//typedef struct point Point;
+//
+//
+//typedef struct point1
+//{
+//	int xpos;
+//	int ypos;
+//}Point1;
+//
+//
+//typedef struct
+//{
+//	int xpos;
+//	int ypos;
+//}Ppin1;
+
+
+//int main(void)
+//{
+//	int a = 10;
+//	int p = &a;
+//
+//	printf("%x\n", p);
+//}
